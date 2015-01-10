b02faffb8cd6a79aefeb5ec0edbcadb003d24bb2
skiwi <frank_skiwi@hotmail.com>
2015-01-10 19:30:12 +0100

    Added an extensible monitoring file log reader class.

---
+++ src/main/java/com/github/skiwi2/hearthmonitor/logreader/AbstractMonitoringFileLogReader.java
@@ -0,0 +1,50 @@
+package com.github.skiwi2.hearthmonitor.logreader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Can be extended to read log entries from a log file, blocking until more input is available.
+ *
+ * @author Frank van Heeswijk
+ */
+public abstract class AbstractMonitoringFileLogReader extends AbstractLineLogReader implements CloseableLogReader {
+    private final BufferedReader bufferedReader;
+
+    /**
+     * Initializes an AbstractMonitoringFileLogReader instance.
+     *
+     * @param bufferedReader    The buffered reader from which to read
+     * @throws  java.lang.NullPointerException  If bufferedReader is null.
+     */
+    protected AbstractMonitoringFileLogReader(final BufferedReader bufferedReader) {
+        this.bufferedReader = Objects.requireNonNull(bufferedReader, "bufferedReader");
+    }
+
+    /**
+     * Returns the next line from the log file.
+     *
+     * This method will block until input is available, or an IOException or InterruptedException has occurred.
+     *
+     * @return  The next line from the log file.
+     * @throws NoMoreInputException If an underlying IOException or InterruptedException has been thrown.
+     */
+    @Override
+    protected String readLineFromLog() throws NoMoreInputException {
+        try {
+            String line;
+            while ((line = bufferedReader.readLine()) == null) {
+                Thread.sleep(100);
+            }
+            return line;
+        } catch (IOException | InterruptedException | RuntimeException ex) {
+            throw new NoMoreInputException(ex);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        bufferedReader.close();
+    }
+}
