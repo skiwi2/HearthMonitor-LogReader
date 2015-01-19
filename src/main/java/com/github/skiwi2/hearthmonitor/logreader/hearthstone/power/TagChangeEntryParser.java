6e6f96901e94c94668936fbf76a6caea4e59c70e
skiwi <frank_skiwi@hotmail.com>
2015-01-19 12:43:32 +0100

    Added the TagChangeEntryParser class.

---
+++ src/main/java/com/github/skiwi2/hearthmonitor/logreader/hearthstone/power/TagChangeEntryParser.java
@@ -0,0 +1,57 @@
+package com.github.skiwi2.hearthmonitor.logreader.hearthstone.power;
+
+import com.github.skiwi2.hearthmonitor.logapi.LogEntry;
+import com.github.skiwi2.hearthmonitor.logapi.power.TagChangeLogEntry;
+import com.github.skiwi2.hearthmonitor.logreader.EntryParser;
+import com.github.skiwi2.hearthmonitor.logreader.LineReader;
+import com.github.skiwi2.hearthmonitor.logreader.NotParsableException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Frank van Heeswijk
+ */
+public class TagChangeEntryParser implements EntryParser {
+    /*
+     * [Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=skiwi tag=TIMEOUT value=75
+     */
+
+    @Override
+    public boolean isParsable(final String input) {
+        return input.startsWith("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=");
+    }
+
+    /**
+     * Pattern that checks if a string matches the following:
+     *  - starts with literal text '[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity='
+     *  - followed by one or more word characters, captured as the 1st group
+     *  - followed by literal text ' tag='
+     *  - followed by one or more word characters, captured as the 2nd group
+     *  - followed by literal text ' value='
+     *  - followed by one or more word characters, captured as the 3rd group
+     *  - ending with zero or more characters
+     */
+    private static final Pattern EXTRACT_TAG_CHANGE_PATTERN =
+        Pattern.compile("^" + Pattern.quote("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=") + "(\\w+)"
+             + Pattern.quote(" tag=") + "(\\w+)" + Pattern.quote(" value=") + "(\\w+).*");
+
+    @Override
+    public LogEntry parse(final String input, final LineReader lineReader) throws NotParsableException {
+        TagChangeLogEntry.Builder builder = new TagChangeLogEntry.Builder();
+
+        Matcher tagChangeMatcher = EXTRACT_TAG_CHANGE_PATTERN.matcher(input);
+        if (!tagChangeMatcher.find()) {
+            throw new NotParsableException();
+        }
+        String entity = tagChangeMatcher.group(1);
+        String tag = tagChangeMatcher.group(2);
+        String value = tagChangeMatcher.group(3);
+
+        builder.entity(entity);
+        builder.tag(tag);
+        builder.value(value);
+
+        return builder.build();
+    }
+}
---
+++ src/test/java/com/github/skiwi2/hearthmonitor/logreader/hearthstone/power/TagChangeEntryParserTest.java
@@ -0,0 +1,29 @@
+package com.github.skiwi2.hearthmonitor.logreader.hearthstone.power;
+
+import com.github.skiwi2.hearthmonitor.logapi.power.TagChangeLogEntry;
+import com.github.skiwi2.hearthmonitor.logreader.CloseableLogReader;
+import com.github.skiwi2.hearthmonitor.logreader.logreaders.FileLogReader;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class TagChangeEntryParserTest {
+    @Test
+    public void testTagChange() throws Exception {
+        BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(getClass().getResource("TagChange.log").toURI()), StandardCharsets.UTF_8);
+        try (CloseableLogReader logReader = new FileLogReader(bufferedReader, () -> new HashSet<>(Arrays.asList(new TagChangeEntryParser())))) {
+            TagChangeLogEntry tagChangeLogEntry = (TagChangeLogEntry)logReader.readNextEntry();
+
+            assertEquals("skiwi", tagChangeLogEntry.getEntity());
+            assertEquals("TIMEOUT", tagChangeLogEntry.getTag());
+            assertEquals("75", tagChangeLogEntry.getValue());
+        }
+    }
+}
