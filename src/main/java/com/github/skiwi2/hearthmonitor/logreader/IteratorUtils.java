78ff3efe66170b87d8de29e0c4efd607b3e74814
skiwi <frank_skiwi@hotmail.com>
2015-01-23 18:59:22 +0100

    Added IteratorUtils class to provide complex iterator helper methods.

---
+++ src/main/java/com/github/skiwi2/hearthmonitor/logreader/IteratorUtils.java
@@ -0,0 +1,31 @@
+package com.github.skiwi2.hearthmonitor.logreader;
+
+import java.util.Iterator;
+import java.util.Spliterators;
+import java.util.function.Predicate;
+import java.util.stream.StreamSupport;
+
+/**
+ * Utility class to do complex things with iterators.
+ *
+ * @author Frank van Heeswijk
+ */
+public final class IteratorUtils {
+    private IteratorUtils() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns an iterator that is a view on the given iterator only considering elements that match the condition predicate.
+     *
+     * @param iterator  The input iterator
+     * @param condition The condition predicate that elements have to match
+     * @param <E>   The type of elements
+     * @return  The iterator that is a view on the given iterator only considering elements that match the condition predicate.
+     */
+    public static <E> Iterator<E> filteredIterator(final Iterator<E> iterator, final Predicate<E> condition) {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, 0), false)
+            .filter(condition)
+            .iterator();
+    }
+}
---
+++ src/test/java/com/github/skiwi2/hearthmonitor/logreader/IteratorUtilsTest.java
@@ -0,0 +1,34 @@
+package com.github.skiwi2.hearthmonitor.logreader;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class IteratorUtilsTest {
+    @Test
+    public void testFilteredIterator() {
+        Iterator<String> iterator = Arrays.asList("0", "a", "0", "b", "0", "c", "0").iterator();
+        Iterator<String> filteredIterator = IteratorUtils.filteredIterator(iterator, string -> !string.equals("0"));
+
+        assertTrue(filteredIterator.hasNext());
+        assertEquals("a", filteredIterator.next());
+
+        assertTrue(filteredIterator.hasNext());
+        assertEquals("b", filteredIterator.next());
+
+        assertTrue(filteredIterator.hasNext());
+        assertEquals("c", filteredIterator.next());
+
+        assertFalse(filteredIterator.hasNext());
+        try {
+            filteredIterator.next();
+            fail();
+        } catch (NoSuchElementException ex) {
+            //ok
+        }
+    }
+}
