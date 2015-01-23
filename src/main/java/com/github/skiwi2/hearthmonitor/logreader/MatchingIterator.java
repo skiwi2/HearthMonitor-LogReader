cb8c427ef5f418ebc298feb3d1d00ed48d5c724f
skiwi <frank_skiwi@hotmail.com>
2015-01-23 18:29:49 +0100

    Added matching iterator interface.

---
+++ src/main/java/com/github/skiwi2/hearthmonitor/logreader/MatchingIterator.java
@@ -0,0 +1,76 @@
+package com.github.skiwi2.hearthmonitor.logreader;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+/**
+ * Used to iterate over elements while also having the possibility to check if the next elements matches a predicate.
+ *
+ * @author Frank van Heeswijk
+ * @param <E>   The type of elements
+ */
+public interface MatchingIterator<E> extends Iterator<E> {
+    /**
+     * Returns whether the next element matches the given condition.
+     *
+     * @param condition The condition predicate that the next element may match
+     * @return  Whether the next element matches the given condition.
+     * @throws  java.lang.NullPointerException  If condition is null.
+     */
+    boolean nextMatches(final Predicate<E> condition);
+
+    /**
+     * Returns a matching iterator constructed from an iterator.
+     *
+     * @param iterator  The input iterator
+     * @param <E>   The type of the elements in the iterator
+     * @return  The matching iterator constructed from the iterator.
+     */
+    static <E> MatchingIterator<E> fromIterator(final Iterator<E> iterator) {
+        return new MatchingIterator<E>() {
+            private final List<E> peekedElements = new ArrayList<>();
+
+            @Override
+            public boolean hasNext() {
+                Optional<E> peekElement = peek();
+                return peekElement.isPresent();
+            }
+
+            @Override
+            public E next() {
+                if (!peekedElements.isEmpty()) {
+                    return peekedElements.remove(0);
+                }
+                return iterator.next();
+            }
+
+            @Override
+            public boolean nextMatches(final Predicate<E> condition) {
+                Objects.requireNonNull(condition, "condition");
+                Optional<E> peekElement = peek();
+                return (peekElement.isPresent() && condition.test(peekElement.get()));
+            }
+
+            /**
+             * Returns an optional containing the next element, or an empty optional if there is none.
+             *
+             * @return  The optional containing the next element, or the empty optional if there is none.
+             */
+            private Optional<E> peek() {
+                if (!peekedElements.isEmpty()) {
+                    return Optional.ofNullable(peekedElements.get(0));
+                }
+                if (!iterator.hasNext()) {
+                    return Optional.empty();
+                }
+                E element = iterator.next();
+                peekedElements.add(element);
+                return Optional.ofNullable(element);
+            }
+        };
+    }
+}
---
+++ src/test/java/com/github/skiwi2/hearthmonitor/logreader/MatchingIteratorTest.java
@@ -0,0 +1,43 @@
+package com.github.skiwi2.hearthmonitor.logreader;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class MatchingIteratorTest {
+    @Test
+    public void testFromIterator() {
+        Iterator<String> iterator = Arrays.asList("a", "b", "c").iterator();
+        MatchingIterator<String> matchingIterator = MatchingIterator.fromIterator(iterator);
+
+        assertTrue(matchingIterator.hasNext());
+        assertFalse(matchingIterator.nextMatches(string -> !string.equals("a")));
+        assertTrue(matchingIterator.nextMatches(string -> string.equals("a")));
+        assertEquals("a", matchingIterator.next());
+
+        assertTrue(matchingIterator.hasNext());
+        assertFalse(matchingIterator.nextMatches(string -> !string.equals("b")));
+        assertTrue(matchingIterator.nextMatches(string -> string.equals("b")));
+        assertEquals("b", matchingIterator.next());
+
+        assertTrue(matchingIterator.hasNext());
+        assertFalse(matchingIterator.nextMatches(string -> !string.equals("c")));
+        assertTrue(matchingIterator.nextMatches(string -> string.equals("c")));
+        assertEquals("c", matchingIterator.next());
+
+        assertFalse(matchingIterator.hasNext());
+        assertFalse(matchingIterator.nextMatches(string -> true));
+        assertFalse(matchingIterator.nextMatches(string -> false));
+
+        try {
+            matchingIterator.next();
+            fail();
+        } catch (NoSuchElementException ex) {
+            //ok
+        }
+    }
+}
